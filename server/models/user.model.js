c0d0f6fc870c3aded9adb2dbbb9469de59e215ff
oumatoulacen <oumatoulacen@gmail.com>
2024-04-01 22:46:38 +0000

    add followers and following to user schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -26,6 +26,18 @@ const userSchema = new mongoose.Schema({
             ref: 'Post'
         }
     ],
+    following: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    followers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
 },
 { timestamps: true }
 )
