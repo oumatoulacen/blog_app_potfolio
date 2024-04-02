34d3612825a9ae84163860ed1fc18b1dba2447e9
oumatoulacen <oumatoulacen@gmail.com>
2024-04-02 02:15:32 +0000

    add category to the post

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,9 +6,10 @@ import 'react-quill/dist/quill.snow.css';
 import axios from 'axios';
 
 export default function CreatePost() {
-    const [post, setPost] = useState({ title: '', content: '', image: ''});
+    const [post, setPost] = useState({ title: '', content: '', image: '', category: ''});
     const fileInputRef = useRef(null); // Create a ref for the file input element
     const [notification, setNotification] = useState('');
+    const categories = ['Technology', 'Science', 'Health', 'Sports', 'Music', 'Movies', 'Travel', 'Fashion', 'Food', 'Books', 'Business', 'Art', 'Design', 'Photography', 'Education', 'Fitness', 'Gaming', 'History', 'Nature', 'Politics', 'Religion', 'Space', 'Weather', 'Animals', 'Cars', 'DIY', 'Gardening', 'Home', 'Humor', 'Kids', 'Lifestyle', 'Parenting', 'Relationships', 'Self-Improvement', 'Spirituality', 'Writing', 'Others'];
     const navigate = useNavigate();
 
     // Truncate long words
@@ -35,6 +36,7 @@ export default function CreatePost() {
             formData.append('title', removeUnderscore(post.title));
             formData.append('content', post.content);
             formData.append('image', post.image);
+            formData.append('category', post.category);
             formData.append('userId', localStorage.getItem('userId'));
             console.log('formData: ', formData)
             await axios.post('http://localhost:5000/posts', formData, {
@@ -48,9 +50,7 @@ export default function CreatePost() {
             setPost({ title: '', content: '', image: '' });
             setNotification('Post created successfully!');
             // Clear notification after 3 seconds
-            setTimeout(() => {
-                setNotification('');
-            }, 1000);
+            setNotification('');
             // Redirect to the home page after creating the post
             navigate('/');
         } catch (error) {
@@ -59,7 +59,7 @@ export default function CreatePost() {
     }
 
     return (
-        <div className='flex flex-col justify-center items-center' >
+        <div className='flex flex-col justify-center items-center mx-3'>
             <h1 className="text-2xl font-bold mt-5">Create Post</h1>
             <form onSubmit={handleSubmit} className='p-10 rounded-lg border-2 border-green-600'>
                 {notification && (
@@ -72,10 +72,20 @@ export default function CreatePost() {
                     <input type="text" className="form-input mt-1 block w-full" id="title" name='title' value={post.title} onChange={handleChange} required/>
                 </div>
                 <div className="mb-4">
+                    {/* select a category and handle change by setting post.category */}
+                    <label htmlFor="category" className="block text-gray-700">Category</label>
+                    <select name="category" id="category" className="form-select mt-1 block w-full" value={post.category} onChange={handleChange} required>
+                        <option value="" disabled>Select a category</option>
+                        {categories.map((cat, index) => (
+                            <option key={index} value={cat}>{cat}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className="mb-4">
                     <label htmlFor="content" className="block text-gray-700">Content</label>
                     <ReactQuill theme="snow" className='h-72 mb-12' value={post.content} onChange={handleContent} name='content' id="content" />
                 </div>
-                <div className="mb-4">
+                <div className="my-4">
                     <label htmlFor="image" className="block text-gray-700">Image</label>
                     <input type="file" ref={fileInputRef} className="block w-full text-sm text-slate-500" id="image" name='image' onChange={handleChange} required/>
                 </div>
