1ee4b5c95c2932f7b12c5ac615652f8693235498
oumatoulacen <oumatoulacen@gmail.com>
2024-04-03 01:15:50 +0000

    make cookies optional

--- server/middlewares/authCookies.middleware.js
+++ server/middlewares/authCookies.middleware.js
@@ -1,5 +1,5 @@
 const AuthCookies = (req, res, next) => {
-    const token = req.cookies.token
+    const token = req.cookies?.token
 
     if (!token) {
         return res.status(401).send('Unauthorized')
