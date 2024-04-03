b6ffc2f54d86743dce1ce8d25c2ed96c8ebb11ca
oumatoulacen <oumatoulacen@gmail.com>
2024-04-03 01:16:53 +0000

    add comments, likes, category, reads to the post schema

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -12,9 +12,72 @@ const postSchema = new mongoose.Schema({
     image: {
         type: String,
     },
-    user: {
+    userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            defaultValue: []
+        }
+    ],
+    comments: [
+        {
+            text: String,
+            userId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now
+            },
+            updatedAt: {
+                type: Date,
+                default: Date.now
+            },
+            likes: [
+                {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'User',
+                    defaultValue: []
+                }
+            ],
+            replies: [
+                {
+                    text: String,
+                    user: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: 'User'
+                    },
+                    createdAt: {
+                        type: Date,
+                        default: Date.now
+                    },
+                    updatedAt: {
+                        type: Date,
+                        default: Date.now
+                    },
+                    likes: [
+                        {
+                            type: mongoose.Schema.Types.ObjectId,
+                            ref: 'User',
+                            defaultValue: []
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    category: {
+        type: String,
+        default: 'others'
+    },
+    reads: {
+        type: Number,
+        default: 0
     }
   }, { timestamps: true }
 )
