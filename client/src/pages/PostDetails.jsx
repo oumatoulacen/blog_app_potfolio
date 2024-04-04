3e39144eda584b4d9f2081d20fcf7712bea669ad
oumatoulacen <oumatoulacen@gmail.com>
2024-04-04 04:43:29 +0000

    add post interaction icon and numbers plus some small changes

--- client/src/pages/PostDetails.jsx
+++ client/src/pages/PostDetails.jsx
@@ -11,9 +11,7 @@ function getImageUrl(name) {
 
     const [likes, setLikes] = useState([])
     const [reads, setReads] = useState(0)
-    const [read, setRead] = useState(false)
     const [user, setUser] = useState({})
-    const [liked, setLiked] = useState(false)
     const [post, setPost] = useState({ postId: '', title: '', content: '', image: '', userId: '', category: '', createdAt: '', likes: [], reads: 0})
     const [comment, setComment] = useState('')
     const [comments, setComments] = useState([])
@@ -36,8 +34,8 @@ function getImageUrl(name) {
         getComments()
     }, [postId])
 
+    // Focus on the comment input when the URL contains "#comments"
     useEffect(() => {
-      // Focus on the comment input when the URL contains "#comments"
       if (location.hash === '#comments') {
         commentInputRef.current.focus();
       }
@@ -49,7 +47,6 @@ function getImageUrl(name) {
             try {
                 const res = await axios.get(`http://localhost:5000/posts/${postId}`)
                 setPost(res.data)
-                console.log('post: ', res.data)
             } catch (err) {
                 console.log(err)
             }
@@ -62,32 +59,30 @@ function getImageUrl(name) {
     useEffect(() => {
         setLikes(post.likes?.length)
         setReads(post.reads)
-        }, [post.likes?.length, post.reads])
+        }, [post.likes.length, post.reads])
     
     // set the user
     useEffect(() => {
         const getUser = async () => {
             try {
-                const res = await axios.get(`http://localhost:5000/users/${activeUserId}`)
+                const res = await axios.get(`http://localhost:5000/users/${post.userId._id}`)
                 setUser(res.data)
             } catch (err) {
                 console.log(err)
             }
         }
         getUser()
-    }, [activeUserId])
+    }, [post.userId])
 
-    const handleLikes = async () => {
+    const handleLikes =  () => {
+        if (!activeUserId) {
+            window.location.href = '/sign-in'
+        }
         try {
-            if (liked) {
-                console.log('liked', likes)
-                setLiked(true)
-            } else {
-                console.log('liked not', likes)
-                setLiked(false)
-            }
-            const res = await axios.put(`http://localhost:5000/posts/${postId}/likes/${activeUserId}`)
-            setLikes(res.data.likes.length)
+            axios.put(`http://localhost:5000/posts/${postId}/likes/${activeUserId}`)
+            .then(res => {
+                setLikes(res.data.likes.length)
+            })
         } catch (err) {
             console.log(err.message)
         }
@@ -95,13 +90,8 @@ function getImageUrl(name) {
     
     const handleReads = async () => {
         try {
-            if (read) {
-                navigate(`/posts/${postId}`)
-                return
-            }
             const res = await axios.put(`http://localhost:5000/posts/${postId}/reads/${activeUserId}`)
             setReads(res.data.reads)
-            setRead(true)
             // navigate to the post page
             navigate(`/posts/${postId}`)
         } catch (err) {
@@ -118,6 +108,9 @@ function getImageUrl(name) {
 
 
     const addComment = async () => {
+        if (!activeUserId) {
+            window.location.href = '/sign-in'
+        }
         try {
             const res = await axios.put(`http://localhost:5000/posts/${postId}/comment`, { text: comment, userId: activeUserId })
             setComments(res.data)
@@ -136,17 +129,17 @@ function getImageUrl(name) {
                     <div className="flex flex-col gap-6">
                         <div className="flex flex-row items-center justify-between">
                             <div className="flex flex-row items-center justify-start gap-3">
-                                <a className="" href="/">
+                                <Link className="" to="/">
                                     <div className="flex items-center justify-center bg-slate-100 cursor-pointer relative w-10 h-10 rounded-full overflow-hidden">
                                         <div className="">
-                                            <img alt={user.username} src={getImageUrl(user.avatar)} decoding="async" data-nimg="fill" className="" loading="lazy"/>
+                                            <img alt={user.username} src={getImageUrl(user.avatar /* post.userId.avatar */)} decoding="async" data-nimg="fill" className="" loading="lazy"/>
                                         </div>
                                     </div>
-                                </a>
+                                </Link>
                                 <div className="flex flex-col">
                                     <div className="flex flex-row justify-start items-center text-sm gap-1">
                                         <div className="flex gap-2">
-                                                <span className="font-semibold text-slate-700 dark:text-slate-200 cursor-pointer">{ user.username }</span>
+                                                <span className="font-semibold text-slate-700 dark:text-slate-200 cursor-pointer">{ user.username /*post.userId.username*/ }</span>
                                         </div>
                                     </div>
                                     <div className="flex flex-row items-center justify-start gap-1">
@@ -165,7 +158,7 @@ function getImageUrl(name) {
                             <div>
                                 <h1 className="font-heading text-base sm:text-xl font-semibold sm:font-bold  text-slate-700 dark:text-slate-200 hn-break-words cursor-pointer">{post.title}</h1>
                             </div>
-                            <div className="hidden md:block">
+                            <div className="md:block">
                                 <span
                                     className="text-base  font-normal text-slate-500 dark:text-slate-400 hn-break-words cursor-pointer"
                                     dangerouslySetInnerHTML={{ __html: post.content }}
@@ -178,21 +171,21 @@ function getImageUrl(name) {
                             <div className="md:hidden">
                                 <div data-radix-aspect-ratio-wrapper="" style={{ position: "relative", width: "100%", paddingBottom: "56.25%" }}>
                                     <div style={{ position: "absolute", inset: "0px" }}>
-                                        <a className="block w-full h-full overflow-hidden rounded-xl md:rounded-lg focus:outline-none focus:ring focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 focus:dark:ring-offset-slate-800" href="/">
+                                        <Link className="block w-full h-full overflow-hidden rounded-xl md:rounded-lg focus:outline-none focus:ring focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 focus:dark:ring-offset-slate-800" to="/">
                                             <span style={{ boxSizing: "border-box", display: "block", overflow: "hidden", width: "initial", height: "initial", background: "none", opacity: 1, border: "0px", margin: "0px", padding: "0px", position: "absolute", inset: "0px" }}>
                                                 <img alt="Architecture of my collaborative brainstorming app" src={getImageUrl(post.image)}  style={{ position: "absolute", inset: "0px", boxSizing: "border-box", padding: "0px", border: "none", margin: "auto", display: "block", width: "0px", height: "0px", minWidth: "100%", maxWidth: "100%", minHeight: "100%", maxHeight: "100%", objectFit: "cover" }} />
                                             </span>
-                                        </a>
+                                        </Link>
                                     </div>
                                 </div>
                             </div>
                             {/* show in middle and lar  ge */}
                             <div className="hidden md:block w-full h-full">
-                            <a className="block w-full h-full overflow-hidden rounded-xl md:rounded-lg focus:outline-none focus:ring focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 focus:dark:ring-offset-slate-800" href="">
+                            <Link className="block w-full h-full overflow-hidden rounded-xl md:rounded-lg focus:outline-none focus:ring focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 focus:dark:ring-offset-slate-800" to="">
                                 <span style={{ boxSizing: "border-box", display: "block", overflow: "hidden", width: "initial", height: "initial", background: "none", opacity: 1, border: "0px", margin: "0px", padding: "0px", position: "absolute", inset: "0px" }}>
-                                <img alt={post.title} src={getImageUrl(post.image)} className="css-5eln6m" />
+                                    <img alt={post.title} src={getImageUrl(post.image)} className="css-5eln6m" />
                                 </span>
-                            </a>
+                            </Link>
                             </div>
                         </div>
                         </div>
@@ -203,27 +196,39 @@ function getImageUrl(name) {
             <section className="flex flex-col gap-5">
                 <div className="flex flex-row items-center justify-between text-slate-600 dark:text-slate-300 text-sm">
                     <div className="flex flex-row items-center justify-start gap-2">
-                        <Link href={`/posts/${postId}`}>
+                        <Link to={`/posts/${postId}#comments`}>
+                            {/* discuss icon as svg and number of comments*/}
                             <div className="group flex">
                                 <svg fill="none" viewBox="0 0 20 20" width="20" height="20">
                                     <path stroke="currentColor" d="M8.709 14.155a4.793 4.793 0 0 1 5.412-6.55m-5.412 6.55a4.793 4.793 0 0 0 6.31 2.54c.1-.044.21-.06.317-.042l2.213.37c.18.03.337-.127.307-.307l-.371-2.21a.566.566 0 0 1 .041-.316 4.793 4.793 0 0 0-3.405-6.586m-5.412 6.55a5.845 5.845 0 0 1-2.682-.461.689.689 0 0 0-.385-.05l-2.695.45a.324.324 0 0 1-.373-.373l.452-2.69a.689.689 0 0 0-.05-.386 5.835 5.835 0 0 1 9.482-6.435 5.808 5.808 0 0 1 1.663 3.395" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.25">
                                     </path>
                                 </svg>
+                                <span className="discuss pl-1">{ post.comments?.length }</span>
                                 <span className="discuss pl-1">Discuss</span>
                             </div>
                         </Link>
                         <p className="font-bold text-slate-400 dark:text-slate-500">·</p>
-                        <p onClick={handleLikes}>{likes} likes</p>
+                        {/* like icon as svg and number of likes*/}
+                        <div className="group flex">
+                            <svg onClick={handleLikes} className='cursor-pointer' xmlns="http://www.w3.org/2000/svg" width='20' height='20' viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
+                                <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3">
+                                </path>
+                            </svg>
+                            <span className="discuss pl-1">{likes}</span>
+                            <span className="discuss pl-1">Likes</span>
+                        </div>
+                       
+                        {/* <p onClick={handleLikes}>{likes} likes</p> */}
                         <p className="font-bold text-slate-400 dark:text-slate-500">·</p>
                         <p>{reads} reads</p>
                     </div>
                     <div className="flex-row items-center flex gap-1">
                         <div className="hidden sm:flex gap-2 items-center">
-                            <a href={`/categories/${post.category}` }>
+                            <Link to={`/categories/${post.category}` }>
                                 <div className="flex justify-start items-center rounded-full px-2 py-1 cursor-pointer text-xs font-medium text-slate-600 dark:text-slate-300 bg-slate-100 hover:bg-slate-200 dark:bg-slate-900 dark:hover:bg-slate-700 w-min max-w-[126px] truncate text-left">
                                     <span className="truncate">{post.category}</span>
                                 </div>
-                            </a>
+                            </Link>
                         </div>
                     </div>
                 </div>
