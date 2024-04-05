dd6fedea48d5cb4ea2a602f45791f9e23a73eeab
oumatoulacen <oumatoulacen@gmail.com>
2024-04-05 10:58:32 +0000

    add edit profile

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -3,13 +3,17 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import avatar from '../assets/avatar.png';
 import Post from '../components/Post';
+import EditProfileModal from '../components/EditProfileModal';
+
 
 function getImageUrl(name) {
     return new URL(`../assets/${name}`, import.meta.url).href
 }
 
+
 const Profile = () => {
     const activeUserId = localStorage.getItem('activeUserId');
+    const [showModal, setShowModal] = useState(false);
     const id = useParams().id;
     // store the current user data
     const [user, setUser] = useState({});
@@ -27,8 +31,9 @@ const Profile = () => {
             .catch(err => console.log(err.message));
     }, [id]);
 
+
     // get the user's ID from the local storage and get the user data
-    useEffect(() => {
+     useEffect(() => {
         if (!activeUserId) {
             window.location.href = '/sign-in';
         } else {
@@ -65,22 +70,29 @@ const Profile = () => {
                 setFollowers(res.data.followers.length || 0);
             }).catch(
                 err => console.log(err.message)
-            );
+                );
+        // reload the page
+        window.location.reload();
     };
 
-    // handle Edit profile
-    const handelEdit = () => {
-        console.log('editing')
-    }
+
+    const handleEdit = () => {
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        // reload the page
+        window.location.reload();
+    };
 
     return (
         <div className='mx-10 md:mx-20 lg:mx-30 xl:mx-40'>
-            {/* user info */}
             <div className="flex mt-10">
                 <div className="flex flex-col">
                     <img src={user ? getImageUrl(user.avatar) : avatar} alt="" className="w-20 h-20 rounded-full" />
                 </div>
-                <div className="flex md:flex-row mt-5 ml-2 gap-4 ">
+                <div className="flex md:flex-row mt-5 ml-5 gap-4 ">
                     <h1 className="flex flex-col font-light">
                         <span className="text-green-600">{posts.length}</span>
                         <span className="">Posts</span>
@@ -96,41 +108,42 @@ const Profile = () => {
                 </div>
             </div>
             <div className="flex flex-col items-start mt-5">
-                <h3 className="text-xl mt-2">Your Name: <span className='pl-4'>{user ? user.username : 'Username'}</span></h3>
-                {/* check if the user is visiting his profile or other users */}
+                <h3 className="text-xl mt-2 px-2 border-b"> Bio: <span className='block pl-10'>{user && user.bio ? user.bio : '(Empty)'}</span></h3>
+                <h3 className="text-xl mt-2 px-2 border-b"> Name: <span className='block pl-10'>{user ? user.username : 'Username'}</span></h3>
+            
+                {/*check if the user is visiting his profile or other users */}
                 {
+
                     activeUserId === id ?
-                        <button className="bg-green-600 text-white p-2 rounded-lg mt-2" onClick={handelEdit}>
-                            Edit Profile
-                        </button> :
-                        <button className="bg-green-600 text-white p-2 rounded-lg mt-2" onClick={handleFollow}>
+                        <div>
+                           {
+                                showModal ?
+                                <>
+                                <EditProfileModal isOpen={showModal} onClose={handleCloseModal} user={user} />
+                                </>
+                                : <button className="bg-green-600 text-white p-2 rounded-lg mt-2" onClick={handleEdit}> Edit Profile</button>
+                            }
+                           
+                        </div>
+                        : <button className="bg-green-600 text-white p-2 rounded-lg mt-2" onClick={handleFollow}>
                             { follow }
                         </button>
                 }
             </div>
-            {/* edit user profile form when edit profile button clicked */}
-            
+          
             {/* user posts */}
             <div className="flex flex-col items-start mt-10 pb-2">
-            {
-                activeUserId === id ?
-                <h2 className="text-xl font-bold">Your Posts</h2> :
-                <h2 className="text-xl font-bold">{user.username}&apos;s Posts</h2>
-            }
+                <h2 className="text-xl font-bold">Your Posts</h2>
             </div>
-            <div className="border-t-2 border-gray-300">
-              
-                <div className="flex flex-col justify-center">
-                    {
-                        posts ?
-                            posts.map(post => (
-                                <Post key={post._id} post={post} />
-                            )) : <p>No posts found</p>
-                    }
+                <div className="border-t-2 border-gray-300">
+                    <div className="flex flex-col justify-center">
+                    {posts?.map(post => (
+                        <Post key={post._id} post={post} />
+                    ))}
+                    </div>
                 </div>
-            </div>
         </div>
-        )
+    );
 };
 
 export default Profile;
